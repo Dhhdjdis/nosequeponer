1b7bb2ec841e548a95a4941d95956b6b0480bda9
Dhhdjdis <97328128+Dhhdjdis@users.noreply.github.com>
2024-11-30 15:39:49 -0300

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,4 @@
+function aceptarPerdon() {
+    // Cambia el texto del párrafo con id "respuesta"
+    document.getElementById("respuesta").textContent = "Gracias por aceptar mis disculpas. Estoy realmente arrepentido.";
+}
